bbb6f35b7184cf332a26632387542bac9817c8aa
Rahmat Hidayat <55992301+rahmathidayat1203@users.noreply.github.com>
2022-06-17 07:37:41 +0700

    repairing dashboard no edit

--- app/src/main/java/com/example/financeappdesain/activity/DashboardPage.kt
+++ app/src/main/java/com/example/financeappdesain/activity/DashboardPage.kt
@@ -1,14 +1,16 @@
 package com.example.financeappdesain.activity
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.financeappdesain.R
+import com.google.android.material.R.*
+import com.google.android.material.shape.MaterialShapeDrawable
+
 
 class DashboardPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard_page)
+
     }
 }
